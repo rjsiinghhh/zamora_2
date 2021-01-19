b0998757ea489ab7cd9773ac7af8110157f4d2fd
rjsiinghhh <rjsiinghhh@gmail.com>
2021-01-18 17:40:43 -0800

    need to get the right colors for the calender

--- client/src/schedule/pages/Schedule.js
+++ client/src/schedule/pages/Schedule.js
@@ -7,10 +7,10 @@ import SCalendar from '../components/Calendar'
 const Schedule = props => {
     return(
         <>
-        <SCalendar />
+        
         <h1> Schedule type stuff here</h1>
         <h3>will call to the api i built in the backend DATE and DESCRIPTION</h3>
-        
+        <SCalendar />
         </>
     )
 }
