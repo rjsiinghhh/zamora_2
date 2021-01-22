f511c239a539cc3011797749940ba86396c85ae7
rjsiinghhh <rjsiinghhh@gmail.com>
2021-01-21 17:41:46 -0800

    added another table to the data bases for the expenses page

--- server/data.sql
+++ server/data.sql
@@ -1,34 +1,9 @@
-CREATE DATABASE zamora;
-
-\c zamora;
-
-CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY,
+CREATE TABLE expenses(
+    ex_id SERIAL PRIMARY KEY,
     date DATE, 
-    description VARCHAR(255), 
-    icon VARCHAR(10)
+    price NUMERIC, 
+    category VARCHAR
 );
 
 
-INSERT INTO todo (date, description, icon) VALUES ('2016-03-02 12:05:00', 'never mind son, i got it', 'work');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-\q;
+INSERT INTO expenses (date, price, category) VALUES ('2016-03-02 12:05:00', 500.25 , 'work');
